2ebf38a3802b024755ab44d351529207528626a1
温远航 <wenyuanhang@shenjianshou.cn>
2018-07-23 17:06:20 +0800

    第一次提交，初始化！

---
+++ src/main/java/com/wyh/App.java
@@ -0,0 +1,13 @@
+package com.wyh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
