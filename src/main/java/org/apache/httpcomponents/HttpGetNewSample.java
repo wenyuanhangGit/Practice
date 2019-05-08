aa0961cf66f9b588038874058b9b7a1cb97f5881
温远航 <w1074673626@163.com>
2019-05-08 17:37:19 +0800

    没有修改

--- src/main/java/org/apache/httpcomponents/HttpGetNewSample.java
+++ src/main/java/org/apache/httpcomponents/HttpGetNewSample.java
@@ -1,25 +1,16 @@
 package org.apache.httpcomponents;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URL;
-import java.util.Locale;
-import java.util.concurrent.atomic.AtomicReference;
-
-import com.sun.org.apache.xalan.internal.xslt.Process;
 import org.apache.http.HttpEntity;
-import org.apache.http.ProtocolVersion;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class HttpGetNewSample {
 
